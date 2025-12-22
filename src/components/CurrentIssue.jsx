9f24861549eaf6967671f26b92c2b7d5f1950de5
arrayresearch7 <arrayresearch7@gmail.com>
2025-12-22 20:03:00 +0530

    Update CurrentIssue.jsx

--- src/components/CurrentIssue.jsx
+++ src/components/CurrentIssue.jsx
@@ -42,10 +42,16 @@ const articles = [
     pdfUrl: "/vol-14/11.pdf",
   },
   {
-  title: 'Exploring the Evolution of FinTech and Technology Acceptance: Bibliometric Analysis of Research Trends and Future Gaps',
-  authors: 'Dr. Rachna Jain, Dr. Shikha Sharma',
-  pages: '10-32',
-  pdfUrl: '/vol-14/10.pdf',
+    title: 'Exploring the Evolution of FinTech and Technology Acceptance: Bibliometric Analysis of Research Trends and Future Gaps',
+    authors: 'Dr. Rachna Jain, Dr. Shikha Sharma',
+    pages: '10-32',
+    pdfUrl: '/vol-14/10.pdf',
+  },
+  {
+    title: 'Agentic AI for Autonomous CI/CD: Towards Self-Adaptive Financial Infrastructure Pipelines',
+    authors: '1st Avinash Reddy Segireddy',
+    pages: '1-9',
+    pdfUrl: '/vol-14/9.pdf',
   },
 ];
 
