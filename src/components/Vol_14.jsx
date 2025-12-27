fa3fd18817e0306aa26fe74d14bd2e8059766f4d
arrayresearch7 <arrayresearch7@gmail.com>
2025-12-27 15:40:31 +0530

    Update Vol_14.jsx

--- src/components/Vol_14.jsx
+++ src/components/Vol_14.jsx
@@ -5,6 +5,14 @@ import Search from "./Search";
 
 const articlesData = [
   {
+     title:
+      "Corporate Governance and Financial Performance: Evidence from Multinational Firms",
+    authors:
+      "Dr Islam LEBCIR, Dr.T.Rani, Dr. Shabbirali S. Thavara",
+    pages: "96–104",
+    pdfUrl: "/vol-14/18.pdf",
+  },
+  {
     title:
       "The Impact of Central Bank Digital Currencies (CBDCs) on Monetary Policy and Financial Stability",
     authors:
